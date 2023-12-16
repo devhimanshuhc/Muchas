ca7586f024ef44105f35cad2a8ac1cd5d2926095
yung-slime <snakescitybigstateindia@gmail.com>
2023-12-16 22:47:38 +0530

    authContext added

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -10,3 +10,13 @@ export type SessionCredsTyp = {
     email: string,
     password: string,
 }
+
+export type GlobalUserAc = {
+    username: string,
+    phNo?: number,
+    email: string,
+    avatar: string,
+    isMerchant?: boolean,
+    accountId: string,
+    location: string[],
+}
